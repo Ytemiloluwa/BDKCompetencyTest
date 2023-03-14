706d733c0aa54cf9c6f1003f6dce780af3bbe7dd
Temi <ytemiloluwa@gmail.com>
2023-03-14 15:01:07 +0100

    Initial Commit

---
+++ BDKiOSProject/BDKiOSProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  BDKiOSProjectApp.swift
+//  BDKiOSProject
+//
+//  Created by Temiloluwa on 14/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct BDKiOSProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
